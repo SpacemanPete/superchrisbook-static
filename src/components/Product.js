ed30f1bed3e9fa3372f3f750cf69fbd3c4129e79
Piotr Butkiewicz <piotrab@gmail.com>
2019-02-27 23:46:21 -0500

    Create Product component

---
+++ src/components/Product.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Product = props => {
+  return (
+    <div>
+      <section id="buy-book">
+        <div className='container section'>
+          <h2>Buy The Book!</h2>
+          <div className="ecwid ecwid-SingleProduct-v2 ecwid-SingleProduct-v2-centered ecwid-Product ecwid-Product-101046579" data-single-product-id="101046579">
+            Add Snipcart Book Product Here
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Product
