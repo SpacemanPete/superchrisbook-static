c8c9d95c0b04ce4a48be4416e3add9e7f10aca96
Piotr Butkiewicz <piotrab@gmail.com>
2019-02-27 23:46:13 -0500

    configure Footer markup and load into Layout component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,23 @@ import React from "react"
 // TODO: Configure Footer markup with Links, attributeion & copyright
 
 const Footer = ({ siteTitle }) => (
-  <Footer>
-    <div>
-      <Link to="https://mpssociety.org" >
-        MPSSociety.org
-      </Link>
-      <div>Copyright and stuff</div>
+  <footer className="footer">
+    <ul>
+      <li>
+        <a href='https://mpssociety.org/' className='footer-link'>mpssociety.org</a>
+      </li>
+      <li>
+        <a href='#buy-book' className='footer-link'>Buy the book</a>
+      </li>
+    </ul>
+    <div className='attribution'>
+      <span>Site design by <a href='https://www.spacemandev.com'>SpacemanDev</a>.</span>
+        <span>Hosting by <a href='https://www.netlify.com'>Netlify.</a></span>
+      <span>Copyright&copy;
+        <script>document.write(new Date().getFullYear())</script>
+      </span>
     </div>
-  </Footer>
+  </footer>
 )
 
 Footer.propTypes = {
