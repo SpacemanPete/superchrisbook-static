1bb632200eca4fce90bdd0a6dcfcbcd78ac854b3
Piotr Butkiewicz <piotrab@gmail.com>
2019-02-27 19:47:04 -0500

    refactor markup and image for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,43 @@ import React from "react"
 // TODO: Configure Header markup with Logo image, Links & Shopping cart Icon 
 
 const Header = ({ siteTitle }) => (
+  // <header>
+  //   <div id="site-logo">
+  //     <h1>
+  //       <Link to="/" >
+  //         {siteTitle}
+  //       </Link>
+  //     </h1>
+  //   </div>
+  //   <div className="menu">
+  //     <Link to="/story">Chris' Story</Link>
+  //     <a href="#purchase" className="purchase">Buy the book!</a>
+  //     <a href="#shopping-cart" className="shopping-cart">Shopping-cart</a>
+  //   </div>
+  // </header>
   <header>
-    <div id="site-logo">
-      <h1>
-        <Link to="/" >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-    <div className="menu">
-      <Link to="/story">Chris' Story</Link>
-      <a href="#purchase" className="purchase">Buy the book!</a>
-      <a href="#shopping-cart" className="shopping-cart">Shopping-cart</a>
-    </div>
+    <nav class="navbar is-transparent">
+      <div class="container">
+        <div class="navbar-brand">
+          <a href="/" class="navbar-item">
+            <figure class="image">
+              <img src="./img/logo.png" alt="Superchris" />
+            </figure>
+          </a>
+        </div>
+        <ul class="navbar-end">
+          <li>
+            <Link to="/story">Chris' Story</Link>
+          </li>
+          <li>
+            <a class="navbar-item" href='#purchase'>Buy the book!</a>
+          </li>
+          <li>
+            <a href="#shopping-cart" className="shopping-cart">Shopping cart</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </header>
 )
 
