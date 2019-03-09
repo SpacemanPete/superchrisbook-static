7d6da68c1e8675f183c69b0d4758fa770c3d5816
Piotr Butkiewicz <piotrab@gmail.com>
2019-03-09 18:25:54 -0500

    remove deprecated old code

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,6 @@ import React from "react"
 // TODO: Configure Header markup with Logo image, Links & Shopping cart Icon 
 
 const Header = ({ siteTitle }) => (
-  // <header>
-  //   <div id="site-logo">
-  //     <h1>
-  //       <Link to="/" >
-  //         {siteTitle}
-  //       </Link>
-  //     </h1>
-  //   </div>
-  //   <div className="menu">
-  //     <Link to="/story">Chris' Story</Link>
-  //     <a href="#purchase" className="purchase">Buy the book!</a>
-  //     <a href="#shopping-cart" className="shopping-cart">Shopping-cart</a>
-  //   </div>
-  // </header>
   <header>
     <nav className="navbar is-transparent">
       <div className="container">
