34c0770bcf9cf73ac5beb71867056b457dfa4eb7
Piotr Butkiewicz <piotrab@gmail.com>
2019-03-10 22:37:23 -0400

    wireup jQuery to support snipcart

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,16 @@
 
 'use strict'
 
+exports.onCreateWebpackConfig = ({
+  actions,
+}) => {
+  const { setWebpackConfig } = actions;
+  setWebpackConfig({
+    externals: {
+      jquery: 'jQuery', // important: 'Q' capitalized
+    }
+  })
+}
+
 exports.createPages = require('./gatsby/create-pages');
 exports.onCreateNode = require('./gatsby/on-create-nodes');
